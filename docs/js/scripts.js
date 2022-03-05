0d95c0fa9ac16d04d4eae09f44661c4933ca2460
Gareth Matson <gmatson@Gareths-Air.lan>
2022-03-04 19:04:15 -0800

    Initial Commit

---
+++ docs/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Coming Soon v1.0.0 (https://sgvjanitorial.com)
+* Copyright 2013-2022 SGV Janitorial
+* Licensed under MIT (https://github.com/StartBootstrap/-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
